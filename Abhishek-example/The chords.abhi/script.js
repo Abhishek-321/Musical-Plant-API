4b1e1af15dfba63d49fe4ced2ea9f1b5f033549d
Abhishek-321 <giri.abhishek13@yahoo.com>
2021-03-11 17:15:24 +0100

    commit no 10, small changes to chords

--- Abhishek-example/The chords.abhi/script.js
+++ Abhishek-example/The chords.abhi/script.js
@@ -6,13 +6,9 @@ document.getElementById("drybutton").addEventListener("click", function(){
    //display the dryness level
 document.getElementById("display").innerHTML = "The Plant is very Dry";  
 });
-document.getElementById("midbutton").addEventListener("click", function(){
-   dryLevel = 1; 
-   //display the dryness level
-document.getElementById("display").innerHTML = "The plant is little dry. But u be the judge"; 
-});
+
 document.getElementById("goodbutton").addEventListener("click", function(){
-   dryLevel = 2; 
+   dryLevel = 1; 
    //display the dryness level
 document.getElementById("display").innerHTML = "The plant is perfect atm"; 
 });
@@ -27,8 +23,8 @@ document.getElementById("myBtn").addEventListener("click", function() {
 
   
 //sad chords in the key of D
-const dm = ["D2", "F2", "A2"];
-const gm = ["G2", "Bb2", "D2"]; 
+const dm = ["D2", "F2", "A3"];
+const gm = ["G2", "Bb3", "D3"]; 
 const am = ["A2", "C2", "E2"]; 
 const minor = [dm, gm, am]; 
 
@@ -36,38 +32,44 @@ const minor = [dm, gm, am];
 var randomMinor = minor[Math.floor(Math.random()*3)];
 
 //happy chords in the key of D
-const cs2 = ["C4", "D4", "G4"]; 
-const hmm = ["F4", "G4", "C4"]; 
-var happy = [cs2, hmm]; 
-var randomMajor = happy[Math.floor(Math.random()*2)]; 
+const D = ["D4", "F#4", "A4"]; 
+const a7 = ["A4", "c#4", "E4", "G4"]; 
+const G = ["G4", "B5", "D5"];
+const e7 = ["E4", "G#4", "B5", "D5"];
+var happy = [D, a7, G, e7]; 
+var randomMajor = happy[Math.floor(Math.random()*4)]; 
 
 
 if(dryLevel==0){  
    
 
-   const synth = new Tone.PolySynth().toDestination();
+   const synth = new Tone.PolySynth();
+   const vol = new Tone.Volume(-12).toDestination();
+   
    // set the attributes across all the voices using 'set'
    //synth.set({ detune: -1200 });
    // play a chord
-   synth.triggerAttackRelease(randomMinor, 0.5);
+   synth.triggerAttackRelease(randomMinor, 0.5).connect(vol);
 
-} 
 
-else if (dryLevel == 1) {
+           
+/*else if (dryLevel == 1) {
    
-   const synth = new Tone.PolySynth().toDestination();
+   const synth = new Tone.PolySynth( ).toDestination();
    // set the attributes across all the voices using 'set'
    //synth.set({ detune: -1200 });
    // play a chord
    synth.triggerAttackRelease(["A4", "C4", "E4"], 0.5);
-   
+  */ 
 } else {
    
-   const synth = new Tone.PolySynth().toDestination();
+   const synth = new Tone.PolySynth()
+   const vol = new Tone.Volume(-12).toDestination();
+   
    // set the attributes across all the voices using 'set'
    //synth.set({ detune: -1200 });
    // play a chord
-   synth.triggerAttackRelease(randomMajor, 0.5);
+   synth.triggerAttackRelease(randomMajor, 0.5).connect(vol);
 
 }; 
 
